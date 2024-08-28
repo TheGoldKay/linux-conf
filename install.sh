a548de0e94df98f987a51cc072d6de1169f17e98
TheGoldKay <jhonnatanwfc@live.com>
2024-08-28 18:54:29 -0300

    brew configs

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 sudo apt-get install htop
 bash miniconda_install.sh
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#- Run these two commands in your terminal to add Homebrew to your PATH:
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/jade/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+#- Install Homebrew's dependencies if you have sudo access:
+sudo apt-get install build-essential
+source ~/.bashrc
 brew update
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 sudo apt install python3-tk tk-dev tk8.6-dev libsqlite3-dev
