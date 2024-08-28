64de9368a7498fc7de94e544bcc5f2beacd114d5
TheGoldKay <jhonnatanwfc@live.com>
2024-08-28 18:15:47 -0300

    homebrew and its first install: pyenv

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 # a cross-platform interactive process viewer
 sudo apt-get install htop
 bash miniconda_install.sh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo apt install python3-tk tk-dev tk8.6-dev libsqlite3-dev
+brew install pyenv
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
