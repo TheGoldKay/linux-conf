8de856b23f56cbbe429c83dc4dec878dda4a1fe0
GoldKay <jhonnatanwfc@live.com>
2024-09-24 21:10:29 -0300

    a better install

---
+++ must-install.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade # Update and upgrade before installing packages
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh # Sync, search and backup shell history with Atuin
+sudo apt install neofetch # good ol' neofetch
+sudo apt install micro # a better text editor than nano
+sudo apt install nala # a better way of doing 'apt'
+sudo apt install htop # a cross-platform interactive process viewer
+sudo apt install gparted # a partition manager
+# some python & dev dependencies
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo apt install python3-tk tk-dev tk8.6-dev libsqlite3-dev
