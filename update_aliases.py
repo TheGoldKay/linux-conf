e67e876cf65748ef1d5c1e6236b543908d821630
GoldKay <jhonnatanwfc@live.com>
2025-01-18 23:05:46 -0300

    aliases on .bashrc now

--- update_aliases.py
+++
@@ -1,8 +0,0 @@
-def main():
-    with open ('/home/jade/aliases.sh', 'r') as base:
-        with open ('aliases.sh', 'w+') as backup:
-            for line in base.readlines():
-                backup.write(line)
-    
-if __name__ == '__main__':
-    main()
