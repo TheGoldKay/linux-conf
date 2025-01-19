95717bf667d7492a492d9a7c4b66df9f2518489f
GoldKay <jhonnatanwfc@live.com>
2025-01-19 01:21:23 -0300

    better path handling

--- update_bash_conf.py
+++ update_bash_conf.py
@@ -1,7 +1,7 @@
 import os 
 
 def main():
-    with open('/home/goldkay/.bashrc', 'r') as base:
+    with open(os.path.join(os.path.expanduser('~'), '.bashrc'), 'r') as base:
         with open('bash_conf.sh', 'w+') as backup:
             for new_line in base.readlines():
                 backup.write(new_line)
