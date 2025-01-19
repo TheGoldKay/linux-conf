73d97762f4a77f0aa1b7ff6bbfd89a051459dbee
GoldKay <jhonnatanwfc@live.com>
2025-01-19 01:13:13 -0300

    full pkg list

--- must-install.sh
+++ must-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# run first: sudo apt install curl && sudo apt install nala # nala is a better way of doing 'apt'
+sudo apt install curl && sudo apt install nala # a better way of doing 'apt'
 sudo nala update && sudo nala upgrade # Update and upgrade before installing packages
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh # Sync, search and backup shell history with Atuin
 sudo nala install neofetch # good ol' neofetch
@@ -9,6 +9,8 @@ sudo nala install gparted # a partition manager
 # some python & dev dependencies
 sudo nala install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 sudo nala install python3-tk tk-dev tk8.6-dev libsqlite3-dev
+# install miniconda (conda -> python environment manager)
+bash miniconda_install.sh
 # steam
 sudo add-apt-repository multiverse
 sudo nala update
@@ -16,3 +18,15 @@ sudo nala install steam
 # zoxide -> a better cd experience
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 sudo nala install fzf # add on for zoxide
+# homebew 
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/goldkay/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# python stuff
+brew update
+sudo nala install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo nala install python3-tk tk-dev tk8.6-dev libsqlite3-dev
+brew install pyenv
+# xplr (terminal file manager written in Rust) + Also additional dependencies
+brew install ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide imagemagick
+brew install xplr
