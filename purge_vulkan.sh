302b4a1271e27774a7e12f25dde0d7dc97738c08
GoldKay <jhonnatanwfc@live.com>
2025-02-03 02:47:05 -0300

    one liner

--- purge_vulkan.sh
+++ purge_vulkan.sh
@@ -1,5 +1,3 @@
-sudo nata install vulkan-tools libvulkan1 libvulkan1:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 vulkan-validationlayers vulkan-validationlayers-dev libvulkan-dev vulkan-tools vulkan-sdk
+sudo nala purge vulkan-tools libvulkan1 libvulkan1:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 vulkan-validationlayers vulkan-validationlayers-dev libvulkan-dev vulkan-tools vulkan-sdk && apt autoremove --purge && sudo apt clean && sudo apt autoclean
 
-sudo apt autoremove --purge
-sudo apt clean
-sudo apt autoclean
+# DO NOT DO THIS UNLESS EXTREMELY NECESSARY
